d2113216bffffb2edf2732f03ccdc7d94290b108
Constantin <constantinu93@gmail.com>
2021-04-02 17:22:07 +0200

    feat: modified History.js

--- components/History.js
+++ components/History.js
@@ -1,14 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Platform, TouchableOpacity} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {receiveEntries, addEntry} from '../actions';
 import {timeToString, getDailyReminderValue} from '../utils/helpers';
 import {fetchCalendarResults} from '../utils/api';
 import {Agenda} from 'react-native-calendars';
+import {white} from '../utils/colors';
+import DateHeader from './DateHeader';
 
-export default function History() {
-  const entries = useSelector(state => state.entries);
+export default function History(props) {
+  const [state, setState] = useState({
+    entries: [],
+    ready: false,
+  });
+
+  const entries = useSelector(_state => _state.entries);
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     fetchCalendarResults()
       .then(entries => dispatch(receiveEntries(entries)))
@@ -17,25 +26,36 @@ export default function History() {
         if (!entries[timeToString()]) {
           dispatch(
             addEntry({
-              [timeToString()]: getDailyReminderValue(),
+              [timeToString()]: [getDailyReminderValue()],
             }),
           );
         }
-      });
-  });
+      })
+      .then(() => setState(() => ({...state, ready: true})));
+  }, []);
+
   const renderItem = ({today, ...metrics}, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today ? (
-        <Text>{JSON.stringify(today)}</Text>
+        <View>
+          <DateHeader date={formattedDate} />
+          <Text style={styles.noDataText}>{today}</Text>
+        </View>
       ) : (
-        <Text>{JSON.stringify(metrics)}</Text>
+        <TouchableOpacity onPress={() => console.log('Pressed!')}>
+          <Text>{JSON.stringify(metrics)}</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
-  const renderEmptyDate = (formattedDate) => {
+
+  const renderEmptyDate = formattedDate => {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate} />
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
     );
   };
@@ -48,3 +68,27 @@ export default function History() {
     />
   );
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+});
